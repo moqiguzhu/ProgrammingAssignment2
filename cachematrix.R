03c98f57008f16f5ad95c99b97322751cc9b03ff
L.M Lei <moqiguzhu@163.com>
2015-03-16 17:43:55 +0800

    Update cachematrix.R
    The first submit!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Computing inversions of matrixs is a time-consuming operation
+# This file defines two functions to speed inversion operation to a certain degree
+# The key idea is caching, that's to say, if the matrix has been computed before, 
+# then, we can return the inversion of this matrix from cache without calling 'solve' function
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# hold the inversion of one matrix and provide four basic functions for 
+# setting and getting operation
+	
+makeCacheMatrix <- function(mat = matrix()) {
+	inv <- NULL
+	set <- function(m) {
+		mat <<- m
+		inv <<- NULL
+	}
+	get <- function() mat
+	setInv <- function(i) inv <- i
+	getInv <- function() inv
+	list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
+# x -- list returned from makeCacheMatrix
+# y -- matrix needed to be computed this time
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# If the matrix is identical with data of last time, return result from cache
+# else compute inversion of this data and update the cache
+cacheSolve <- function(x, y, ...) {
+	if(!is.null(x$getInv()) & identical(y, x$get())) {
+		return(x$getInv())
+	} else {
+		inv <- solve(y)
+		x$setInv(inv)
+		return(inv)
+	}
 }
